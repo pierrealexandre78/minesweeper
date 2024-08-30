e73f2bafb4524e827e075548174cc3a88857fe8d
Pierre-Alexandre Baril <pierrealexstyle@gmail.com>
2024-08-30 14:38:07 +0000

    [add] tests from Aloys

--- tests/test_minesweeper.py
+++ tests/test_minesweeper.py
@@ -31,7 +31,4 @@ def test_restart():
     assert game.get_board() == [[' ' for _ in range(3)] for _ in range(3)]
     assert game.revealed == set()
     assert game.is_winner() == False   
-
-def test_fail():
-    assert False
     
